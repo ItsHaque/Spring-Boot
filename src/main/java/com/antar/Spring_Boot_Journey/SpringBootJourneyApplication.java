9db1bc56768b56ede8cc74d188eb114bb3254000
Antar <eha087@gmail.com>
2026-08-30 12:24:28 +0600

    first spring boot REST endpoint

---
+++ src/main/java/com/antar/Spring_Boot_Journey/SpringBootJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.antar.Spring_Boot_Journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antar/Spring_Boot_Journey/SpringBootJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antar.Spring_Boot_Journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
